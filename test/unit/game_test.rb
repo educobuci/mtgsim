b5d1a50613c500453509629e2bdd19444d09c33f
Eduardo Cobuci <educobuci@gmail.com>
2012-06-18 20:41:04 -0300

    test if the actual card was removed from hand

--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -9,13 +9,13 @@ class GameTest < MiniTest::Unit::TestCase
   
   def test_simple_game_start
     @game.current_player_index = 0
-    assert_equal @game.current_player.hand.size, 7
+    assert_equal 7, @game.current_player.hand.size
     
     @game.turn
     @game.untap
     @game.draw
-    assert_equal @game.hand.size, 8
-    assert_equal @game.current_player.library.size, 52
+    assert_equal 8, @game.hand.size
+    assert_equal 52, @game.current_player.library.size
     
     @game.hand.each do |card|
       #puts card.name
@@ -44,7 +44,8 @@ class GameTest < MiniTest::Unit::TestCase
     land_index = @game.current_player.hand.index { |c| c.kind_of? Cards::Land }
     @game.play land_index
     
-    assert_equal @game.hand.size, 7
-    assert_equal @game.current_player.battlefield.size, 1
+    assert_equal 7, @game.hand.size
+    assert_equal 1, @game.current_player.battlefield.size
+    refute @game.current_player.hand.include?(@game.current_player.battlefield.first)
   end
 end
