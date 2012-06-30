d9d45cfe4de9fe77fb80df0a895de8114defc7fb
Felipe Seixas <seixasfelipe@gmail.com>
2012-06-30 11:04:23 -0300

    do untap phase

--- lib/mtgsim/game.rb
+++ lib/mtgsim/game.rb
@@ -1,37 +1,38 @@
 class Game
   attr_reader :phase
-  attr_accessor :current_player_index
+
+  def initialize(players)
+    @players = players
+  end
   
   def start
-    player1 = Player.new
-    player1.library = player1.deck.shuffle.dup
-    
-    player2 = Player.new
-    player2.library = player2.deck.shuffle.dup
-    
-    @players = [player1, player2]
+
+    @players[0].library = @players[0].deck.shuffle.dup
+    @players[1].library = @players[1].deck.shuffle.dup
     
     #each player sould draw seven cards
     self.draw_card 0, 7
     self.draw_card 1, 7
+
+    @current_player_index = 0
   end
   def current_player
-    @players[current_player_index]
+    @players[@current_player_index]
   end
-  def hand(player=current_player_index)
+  def hand(player=@current_player_index)
     @players[player].hand
   end
-  def draw_card(player=current_player_index, count=1)
+  def draw_card(player=@current_player_index, count=1)
     count.times do
       p = @players[player]
       p.hand << p.library.pop
     end
   end
-  def play_card(card, player=current_player_index)
+  def play_card(card, player=@current_player_index)
     p = @players[player]
     p.battlefield << p.hand.slice!(card)
   end
-  def tap_card(card, player=current_player_index)
+  def tap_card(card, player=@current_player_index)
     p = @players[player]
     c = p.battlefield[card]
     #c.tap_card
@@ -45,6 +46,7 @@ class Game
     @phase = :untap
   end
   def untap
+    self.current_player.battlefield.each {|c| c.untap_card}
     @phase = :upkeep
   end
   def draw
--- test/unit/game_test.rb
+++ test/unit/game_test.rb
@@ -3,12 +3,11 @@ require "mtgsim"
 
 class GameTest < MiniTest::Unit::TestCase
   def setup
-    @game = Game.new
+    @game = Game.new create_players
     @game.start
   end
   
   def test_simple_game_start
-    @game.current_player_index = 0
     assert_equal 7, @game.current_player.hand.size
     
     game_start()
@@ -22,7 +21,6 @@ class GameTest < MiniTest::Unit::TestCase
   end
   
   def test_game_phases
-    @game.current_player_index = 0
 
     @game.turn
     assert_equal @game.phase, :untap
@@ -54,11 +52,32 @@ class GameTest < MiniTest::Unit::TestCase
     @game.tap_card 0
     assert_equal({ black:0, blue: 1, green: 0, red: 0, white: 0 }, @game.current_player.mana_pool)
   end
+
+  def test_game_untap_phase
+
+    players = create_players
+
+    players[0].battlefield << Cards::Island.new
+    players[0].battlefield << Cards::Snapcaster.new
+
+    players[0].battlefield.each { |c| c.tap_card }
+
+    game = Game.new players
+
+    game.start
+    game.untap
+
+    assert_equal false, players[0].battlefield[0].is_tapped?
+    assert_equal false, players[0].battlefield[1].is_tapped?
+  end
   
   private
+
+  def create_players
+    [Player.new, Player.new]
+  end
   
   def game_start
-    @game.current_player_index = 0
     @game.turn
     @game.untap
     @game.draw
