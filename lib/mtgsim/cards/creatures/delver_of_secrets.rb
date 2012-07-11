71257de22374de4da40af44f7d7df87afa221053
Eduardo Cobuci <educobuci@gmail.com>
2012-07-11 00:49:28 -0300

    added card set and id

--- lib/mtgsim/cards/creatures/delver_of_secrets.rb
+++ lib/mtgsim/cards/creatures/delver_of_secrets.rb
@@ -2,10 +2,11 @@ module Cards
   class DelverofSecrets < Card
     include Creature
     def initialize
-      super "Delver of Secrets", :innistrad, 213
+      super "Delver of Secrets", :innistrad, 51
       self.power = 2
       self.toughness = 1
       self.cost = { blue: 1 }
+      self.types = [:human, :wizard]
     end
   end
 end
--- lib/mtgsim/cards/creatures/geist_of_saint_traft.rb
+++ lib/mtgsim/cards/creatures/geist_of_saint_traft.rb
@@ -2,7 +2,7 @@ module Cards
   class GeistofSaintTraft < Card
     include Creature
     def initialize
-      super "Geist of Saint Traft"
+      super "Geist of Saint Traft", :innistrad, 213
       self.power = 2
       self.toughness = 2
       self.cost = { blue: 1, colorless: 1, white: 1 }
